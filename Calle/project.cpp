f03cf895fbfe1de411b357596e72d4be4ac2c0e0
hurvan <43351280+hurvan@users.noreply.github.com>
2018-11-09 14:31:26 +0100

    Putting in the tempSpectrogram() method

--- Calle/project.cpp
+++ Calle/project.cpp
@@ -7,7 +7,8 @@ void project() {
 	string pathToFile = "/home/calle/MNXB01/Project/datasets/uppsala_tm_1722-2013.dat"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
 	
-	t.tempOnDay(8, 23, 20); //Call some functions that you've implemented
+	//t.tempOnDay(8, 23, 20); //Call some functions that you've implemented
+	t.tempSpectrogram();
 	//t.tempOnDay(235);
 	//t.tempPerDay();
 	//t.hotCold();
