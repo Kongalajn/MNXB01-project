c990c8aceaed498f0ad8e3f00abbe7efdee09eee
hurvan <43351280+hurvan@users.noreply.github.com>
2018-11-10 01:41:59 +0100

    Adding the call of the method
    Just adding the method call. Also some comments describing what it does.

--- Calle/project.cpp
+++ Calle/project.cpp
@@ -1,14 +1,28 @@
 #include "tempTrender.h"
 #include <string>
 
-void project();
-
 void project() {
-	string pathToFile = "/home/calle/MNXB01/Project/datasets/uppsala_tm_1722-2013.dat"; //Put the path to your data file here
+	string pathToFile = "/home/hurvan/MNXB01_project/MNXB01-project/datasets/smhi-opendata_Umea.csv"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
 	
-	//t.tempOnDay(8, 23, 20); //Call some functions that you've implemented
-	t.tempSpectrogram();
+	//t.tempOnDay(8, 23); //Call some functions that you've implemented
+
+
+	// The DataView3D(Int_t startYear, Int_t endYear, bool isUppsala, Int_t startTime=666, Int_t endTime=666)
+	// plots a 2D/3D graph that shows the data points in a window specified by the user.
+	// The first two arguments specify between which year to look, the third argument is true if
+	// you want to look at the uppsala data, else its false.
+	// The two last arguments are only for the CSV files. There you can specify which timewindow
+	// of the day you want to view datapoints from.
+	//
+	// There seems to be some very mysterious problem with how the range of the axis are set, sometimes
+	// it works and sometimes it doesnt. I have not been able to figure out why this is. 
+
+	//t.DataView3D(1722,2014,true); //if uppsala just specify start year and end year
+	t.DataView3D(1962,2015,false,10,17); //if other files, the last two args are the timewindow of the days
+
+
+
 	//t.tempOnDay(235);
 	//t.tempPerDay();
 	//t.hotCold();
