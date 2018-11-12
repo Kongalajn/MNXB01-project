5ed367442991c0167aad00fac31d550978b85b21
hurvan <43351280+hurvan@users.noreply.github.com>
2018-11-12 14:42:16 +0100

    Tested final code

--- Calle/project.cpp
+++ Calle/project.cpp
@@ -1,14 +1,13 @@
 #include "tempTrender.h"
 #include <string>
 
-void project();
-
 void project() {
-	string pathToFile = "/home/courseuser/MNXB01-project/datasets/smhi-opendata_Lulea.csv"; //Put the path to your data file here
+	string pathToFile = "/home/hurvan/MNXB01_project/MNXB01-project/datasets/smhi-opendata_Lulea.csv"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
 	
 	t.tempOnDay(31, 12, 2); //Call some functions that you've implemented
-	
+
+
 	// The DataView3D(Int_t startYear, Int_t endYear, bool isUppsala, Int_t startTime=666, Int_t endTime=666)
 	// plots a 2D/3D graph that shows the data points in a window specified by the user.
 	// The first two arguments specify between which year to look, the third argument is true if
@@ -24,7 +23,8 @@ void project() {
 
 	//t.DataView3D(1722,2014,true); //if uppsala just specify start year and end year
 	t.DataView3D(1962,2015,false,10,17); //if other files, the last two args are the timewindow of the days
-	
-	
-	t.tempPerYear(2050);
+
+
+
+	//t.tempPerYear(2050);
 }
